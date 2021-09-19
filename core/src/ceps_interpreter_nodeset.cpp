a99cce95ac547b9b9f542a1dc065c8945d58eef7
Tomas Prerovsky <cepsdev@hotmail.com>
2021-09-19 17:04:35 +0200

    [FEATURE] nodeset.slice([start index],[stop index(exclusive)]): selects objects from [start index, [Cto stop index

--- core/src/ceps_interpreter_nodeset.cpp
+++ core/src/ceps_interpreter_nodeset.cpp
@@ -1,26 +1,18 @@
-/**
- The MIT License (MIT)
+/*
+Copyright 2021 Tomas Prerovsky (cepsdev@hotmail.com).
 
-Copyright (c) 2014 The authors of ceps
+Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
 
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
+       http://www.apache.org/licenses/LICENSE-2.0
 
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
- **/
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+*/
 
 #include "ceps_interpreter.hh"
 #include "ceps_interpreter_nodeset.hh"
@@ -200,7 +192,32 @@ ceps::ast::Nodebase_ptr ceps::interpreter::evaluate_nodeset_expr_dot(	ceps::ast:
 				v.push_back(result.nodes()[1]);
 				result.nodes_ = v;
 				}
-			} else if (method_name == "at" && args.size() == 1){
+			} else if (method_name == "slice"){
+				std::vector<ceps::ast::Nodebase_ptr> t;
+				if(!args.size()){
+					// no op
+				} else if( 1 == args.size()){
+					 auto range_start = is_int(args[0]);
+					 if(!range_start.first) throw ceps::interpreter::semantic_exception{nullptr,"'"+method_name+"' expects integer as parameter."};
+					 if ((size_t)range_start.second < result.nodes().size()){
+						 std::copy(result.nodes().begin()+range_start.second, result.nodes().end(),std::back_inserter(t));				 
+					 }
+					 result.nodes_ = t;
+				} else {
+					 auto range_start = is_int(args[0]); 
+					 auto range_end = is_int(args[1]);
+					 if(!range_start.first) throw ceps::interpreter::semantic_exception{nullptr,"'"+method_name+"' expects integer as parameter."};
+					 if(!range_end.first) throw ceps::interpreter::semantic_exception{nullptr,"'"+method_name+"' expects integer as parameter."};
+					 if ((size_t)range_start.second < result.nodes().size()){
+						 if (range_start.second <= range_end.second )
+						  if(range_end.second < result.nodes().size()) 
+						   std::copy(result.nodes().begin()+range_start.second, result.nodes().begin()+range_end.second,std::back_inserter(t));
+						 else
+						  std::copy(result.nodes().begin()+range_start.second, result.nodes().end(),std::back_inserter(t));
+					 }
+					 result.nodes_ = t;
+				} 
+            } else if (method_name == "at" && args.size() == 1){
 			  auto r = is_int(args[0]);
 
               if(!r.first) throw ceps::interpreter::semantic_exception{nullptr,"'"+method_name+"' expects integer as parameter."};
